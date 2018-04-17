d4f5feb0c31f2d5afe7ba942ccc8d389fe5268cf
Luis <luisdiazvenero@gmail.com>
2018-04-17 11:39:56 -0500

    obteniendo latitud y longitud

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 const nombre = "Luis";
 
-console.log(nombre);
+window.addEventListener("load", ()=>{
+  // Geolocataion => Navigator
+  if(navigator.geolocation){
+    // Si tienen la api de geolocalizacion
+    navigator.geolocation.getCurrentPosition(function(localizacion) {
+      // localizacion contiene la latitud y longitud en la que se encuentra el usuario
+      console.log(localizacion);
+      console.log("Latitud: " + localizacion.coords.latitude);
+      console.log("Longitud: " + localizacion.coords.longitude);
+    })
+  } else {
+    alert("Tu navegador no soporta las funcionalidades de esta página");
+  }
+
+})
