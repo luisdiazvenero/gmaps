91a1292b4dff55fb2a2108b8654cbe8be8ea3a9e
Luis <luisdiazvenero@gmail.com>
2018-04-17 15:28:08 -0500

    Obteniendo la distancia segun la busqueda

--- src/main.js
+++ src/main.js
@@ -19,9 +19,51 @@ google.maps.event.addDomListener(window,"load",()=>{
     const search_input = document.getElementById("search-place");
     const autocomplete = new google.maps.places.Autocomplete(search_input);
 
+    const marker = new google.maps.Marker({
+      map: map
+    });
+
     autocomplete.bindTo("bounds",map);
-    
+
+    google.maps.event.addListener(autocomplete,"place_changed", ()=>{
+      console.log("cambiamos el lugar");
+
+      const place = autocomplete.getPlace();
+
+      if(place.geometry.viewport){
+        map.fitBounds(place.geometry.viewport);
+      }else{
+        map.setCenter(place.geometry.location);
+        map.setZoom(15);
+      }
+
+      marker.setPlace({
+        placeId: place.place_id,
+        location: place.geometry.location
+      });
+
+      marker.setVisible(true);
+
+      calculateDistance(place,user_location);
+
+    });
+
   });
 
-
 })
+
+function calculateDistance(place, origen){
+  var origin = new google.maps.LatLng(origen.latitude, origen.longitude);
+
+  var service = new google.maps.DistanceMatrixService();
+
+  service.getDistanceMatrix({
+    origins: [origin],
+    destinations: [place.geometry.location],
+    travelMode: google.maps.TravelMode.DRIVING
+  },(respuesta,status)=>{
+    // se ejecuta cuando el servicio de distacia de maps nos responde
+    const distancia = respuesta.rows[0].elements[0];
+    console.log(distancia);
+  })
+}
