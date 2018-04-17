8718b8fd99a2c1c13a7f6385c413047e34895c5f
Luis <luisdiazvenero@gmail.com>
2018-04-17 12:10:55 -0500

    centrar el mapa de acuerdo a la ubicacion de usuario y campo de busqueda

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
 const nombre = "Luis";
 
-window.addEventListener("load", ()=>{
+google.maps.event.addDomListener(window,"load",()=>{
   const user_location = new UserLocation(()=>{
-    console.log("ya tenemos la localizacion yeee");
-    console.log(user_location);
-  })
+    //console.log("ya tenemos la localizacion yeee");
+    //console.log(user_location);
+
+    const mapOptions = {
+        zoom: 14,
+        center: {
+          lat:user_location.latitude,
+          lng:user_location.longitude,
+        }
+    }
+
+    const mapa_element = document.getElementById("map");
+    const map = new google.maps.Map(mapa_element, mapOptions);
+
+    const search_input = document.getElementById("search-place");
+    const autocomplete = new google.maps.places.Autocomplete(search_input);
+
+    autocomplete.bindTo("bounds",map);
+    
+  });
+
 
 })
